e97128332fbf4fe3e0125c7d8cb633be0edf353a
Breno Amorim <brenoamorim11@gmail.com>
2025-07-09 22:23:32 -0300

    basic react setup

---
+++ src/app/pages/home.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+
+
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-12 bg-white">
+      <h1 className="text-4xl font-semibold sm:text-6xl md:text-8xl">
+        Scrapihaus
+      </h1>
+    </main>
+  )
+}
