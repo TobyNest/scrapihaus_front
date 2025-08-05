d9b827166dad988bbb495eb084babb116a98a270
Breno Amorim <brenoamorim11@gmail.com>
2025-08-04 22:37:34 -0300

    fix: vite configo for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/scrapihaus_front',
+  base: '/scrapihaus_front/',
   plugins: [react()],
   resolve: {
     alias: {
