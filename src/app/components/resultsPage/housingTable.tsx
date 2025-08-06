859929ba196c4076880808bcd57018b814b04394
Breno Amorim <brenoamorim11@gmail.com>
2025-08-06 19:37:48 -0300

    fix: refatoring cell components with enum for the cell type, now the values are more readable

--- src/app/components/resultsPage/housingTable.tsx
+++ src/app/components/resultsPage/housingTable.tsx
@@ -1,4 +1,5 @@
 import { Housing } from '@/app/types/housing'
+import { CellType } from '@/utils/env/cellTypeEnum'
 import { faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -7,16 +8,17 @@ interface HousingListProps {
 }
 
 const TableInfoTitle = [
-  { titulo: 'Data de coleta', flex: 3 },
+  { titulo: 'Data de coleta', flex: 4 },
   { titulo: 'Área Privativa', flex: 3 },
   { titulo: 'Tipo', flex: 3 },
+  { titulo: 'Bairro', flex: 3 },
   { titulo: 'Endereço', flex: 8 },
   { titulo: 'Quartos', flex: 2 },
   { titulo: 'Banheiros', flex: 2 },
   { titulo: 'Vagas', flex: 2 },
   { titulo: 'Preço', flex: 3 },
-  { titulo: 'Preço m²', flex: 2 },
-  { titulo: 'IPTU', flex: 2 },
+  { titulo: 'Preço m²', flex: 3 },
+  { titulo: 'IPTU', flex: 3 },
   { titulo: 'Condomínio', flex: 3 },
   { titulo: 'Link', flex: 2 }
 ]
@@ -58,67 +60,80 @@ export function HousingRow({
         value={housing.data_coleta}
         flex={TableInfoTitle[0].flex}
         index={index}
+        cell={CellType.DATA}
       />
       <HousingRowCell
         value={housing.area_privativa}
         flex={TableInfoTitle[1].flex}
         index={index}
-        suffix="m²"
+        cell={CellType.METRO_QUADRADO}
       />
       <HousingRowCell
         value={housing.tipo}
         flex={TableInfoTitle[2].flex}
         index={index}
+        cell={CellType.TEXTO}
+      />
+      <HousingRowCell
+        value={housing.bairro}
+        flex={TableInfoTitle[3].flex}
+        index={index}
+        cell={CellType.TEXTO}
       />
       <HousingRowCell
         value={housing.endereco}
-        flex={TableInfoTitle[3].flex}
+        flex={TableInfoTitle[4].flex}
         index={index}
+        cell={CellType.TEXTO}
       />
       <HousingRowCell
         value={housing.quartos}
-        flex={TableInfoTitle[4].flex}
+        flex={TableInfoTitle[5].flex}
         index={index}
+        cell={CellType.NUMEROS}
       />
       <HousingRowCell
         value={housing.banheiros}
-        flex={TableInfoTitle[5].flex}
+        flex={TableInfoTitle[6].flex}
         index={index}
+        cell={CellType.NUMEROS}
       />
       <HousingRowCell
         value={housing.vagas_garagem}
-        flex={TableInfoTitle[6].flex}
+        flex={TableInfoTitle[7].flex}
         index={index}
+        cell={CellType.NUMEROS}
       />
       <HousingRowCell
         value={housing.valor_total}
-        flex={TableInfoTitle[7].flex}
+        flex={TableInfoTitle[8].flex}
         index={index}
-        prefix="R$"
+        cell={CellType.DINHEIRO}
       />
       <HousingRowCell
         value={housing.valor_m2}
-        flex={TableInfoTitle[8].flex}
+        flex={TableInfoTitle[9].flex}
         index={index}
-        prefix="R$"
+        cell={CellType.DINHEIRO}
       />
       <HousingRowCell
         value={housing.iptu}
-        flex={TableInfoTitle[9].flex}
+        flex={TableInfoTitle[10].flex}
         index={index}
-        prefix="R$"
+        cell={CellType.DINHEIRO}
       />
       <HousingRowCell
         value={housing.condominio}
-        flex={TableInfoTitle[10].flex}
+        flex={TableInfoTitle[11].flex}
         index={index}
-        prefix="R$"
+        cell={CellType.DINHEIRO}
       />
       <HousingRowCell
         value={'Acessar'}
-        flex={TableInfoTitle[11].flex}
+        flex={TableInfoTitle[12].flex}
         index={index}
         link={housing.link}
+        cell={CellType.LINK}
       />
     </div>
   )
@@ -128,17 +143,62 @@ export function HousingRowCell({
   value,
   flex,
   index,
-  prefix,
-  suffix,
-  link
+  link,
+  cell
 }: {
   value: any
   flex: number
   index: number
-  prefix?: string
-  suffix?: string
   link?: string
+  cell: CellType
 }) {
+  let newValue = value
+
+  switch (cell) {
+    case CellType.DATA:
+      newValue = new Date(value).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+      break
+
+    case CellType.METRO_QUADRADO:
+      newValue = value + ' m²'
+      break
+
+    case CellType.TEXTO:
+      newValue = value || '-'
+      break
+
+    case CellType.NUMEROS:
+      newValue = value || '-'
+      break
+
+    case CellType.DINHEIRO:
+      const numero =
+        typeof value === 'string'
+          ? parseFloat(value.replace(',', '.'))
+          : Number(value)
+
+      newValue =
+        numero && numero !== 0
+          ? new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              maximumFractionDigits: 2
+            }).format(numero)
+          : ' - '
+      break
+
+    case CellType.LINK:
+      newValue = 'Acessar'
+      break
+  }
+
   return (
     <div
       style={{ flex }}
@@ -150,8 +210,7 @@ export function HousingRowCell({
         }}
         className={`${link && 'group hover:cursor-pointer hover:font-semibold'} flex h-full items-center pl-2`}
       >
-        {value == 0 ? '' : prefix} {value == 0 ? '-' : value}{' '}
-        {value == 0 ? '' : suffix}
+        {newValue}
         {link && (
           <div className="h-full w-full translate-x-[20px] p-2 opacity-0 transition-all duration-200 ease-in-out group-hover:translate-x-0 group-hover:opacity-100">
             <FontAwesomeIcon icon={faSquareArrowUpRight} />
---
+++ src/utils/env/cellTypeEnum.ts
@@ -0,0 +1,8 @@
+export enum CellType {
+  DATA = 'data',
+  METRO_QUADRADO = 'metro_quadrado',
+  TEXTO = 'texto',
+  NUMEROS = 'numeros',
+  DINHEIRO = 'dinheiro',
+  LINK = 'link'
+}
